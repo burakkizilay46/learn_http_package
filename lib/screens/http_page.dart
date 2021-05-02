574d17315e137277a2f28c8aefc36f9d871c36d1
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2021-05-03 01:04:43 +0300

    hata veriyor

---
+++ lib/screens/http_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:learn_http_lib/models/PostModel.dart';
+import 'dart:async';
+
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class HttpPage extends StatefulWidget {
+  @override
+  _HttpPageState createState() => _HttpPageState();
+}
+
+class _HttpPageState extends State<HttpPage> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("HTTP PACKAGE"),
+      ),
+      body: Center(
+        child: FutureBuilder(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title); // title kısmında hata veriyor..
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            }),
+      ),
+    );
+  }
+}
