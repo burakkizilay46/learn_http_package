19b524854a566d974a96dbd5284471dc1fd0a264
Burak Kızılay <40871181+burakkizilay46@users.noreply.github.com>
2021-05-05 23:25:45 +0300

    Project is done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learn_http_lib/screens/http_listpage.dart';
-import 'package:learn_http_lib/screens/http_page.dart';
+import 'package:learn_http_lib/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,41 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Fetch Data Example',
-        home: HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text("Http Packages")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HttpPage()));
-                },
-                child: Text("One Data")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HttpListPage()),
-                  );
-                },
-                child: Text("Data List")),
-          ],
-        ),
-      ),
+      title: 'Fetch Data Example',
+      home: HomePage(),
     );
   }
 }
